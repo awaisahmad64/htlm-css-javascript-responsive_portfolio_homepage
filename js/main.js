0594d9f234fa00d1562e92c5977bbbe20e13f4ef
Awais Ahmad <awais6412l@gmail.com>
2022-11-19 19:50:05 +0500

    id function add to make js code scalable

--- js/main.js
+++ js/main.js
@@ -1,24 +1,23 @@
 // these event fire on click 
-document.getElementById('humburger').addEventListener('click',shownav);
-document.getElementById('close').addEventListener('click',hidenav);
+const id = (id)=> document.getElementById(id);
+id('humburger').addEventListener('click',shownav);
+id('close').addEventListener('click',hidenav);
 // this event fire on scroll
 window.addEventListener('scroll', stickyfun);
 // this function will show mob menu
 		function shownav(){
 			console.log("shownav btn ic clicked");
-			var navMenu = document.getElementById('nav-menu');
-			navMenu.classList.add('active-nav-menu');
+			id('nav-menu').classList.add('active-nav-menu');
 		}
 		// this function will hide mob menu
 		function hidenav(){
 			console.log("shownav btn ic clicked");
-			var navMenu = document.getElementById('nav-menu');
-			navMenu.classList.remove('active-nav-menu');
+			id('nav-menu').classList.remove('active-nav-menu');
 		}
 		window.addEventListener('scroll', stickyfun);
   // this function will add class sticky when window a bit scroll
 		function stickyfun(){
-			var header = document.getElementById("header");
+			const header = id("header");
 			if(window.scrollY > 0){
                 header.classList.add("sticky");
 			}
